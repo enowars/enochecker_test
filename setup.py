c3c0231812dc63a854c0dcc17bdbcb4efc268e4c
Lucas Druschke <ldruschk@posteo.de>
2021-04-23 16:07:52 +0200

    bump to 0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker_test",
-    version="0.2.0",
+    version="0.3.0",
     author="ldruschk",
     author_email="ldruschk@posteo.de",
     description="Library to help testing checker scripts based on enochecker",
