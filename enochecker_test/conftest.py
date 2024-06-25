a64df0e15a9b21a6cae882c94ef6004df38a7b5c
RTUnreal <unreal@rtinf.net>
2024-06-26 00:33:32 +0200

    add option to skip non standard flags

--- enochecker_test/conftest.py
+++ enochecker_test/conftest.py
@@ -2,6 +2,7 @@ def pytest_addoption(parser):
     parser.addoption("--checker-address", action="store", type=str)
     parser.addoption("--checker-port", action="store", type=int)
     parser.addoption("--service-address", action="store", type=str)
+    parser.addoption("--skip-non-eno-flags", action="store_true")
     parser.addoption("--flag-variants", action="store", type=int)
     parser.addoption("--noise-variants", action="store", type=int)
     parser.addoption("--havoc-variants", action="store", type=int)
